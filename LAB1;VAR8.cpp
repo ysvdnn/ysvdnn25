ac4692c4e97be06e3d31475994dd9f8c3fb82fdc
ysvdnn <yasevich2007@gmail.com>
2025-09-26 11:50:57 +0200

    1

---
+++ LAB1;VAR8.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+int main()
+{
+	int x;
+	std::cin >> x;
+	int k = 32;
+	int maxk = pow(2, 31) - 1;
+	int mink = pow(2, 31) * (-1);
+	std::cout << "The opposite number for " << x <<": " << (-1) * x << std::endl;
+	std::cout << "The value of the root of the second degree for " << x << ": " << sqrt(x) << std::endl;
+	std::cout << "The value of the root of the fifth degree for " << x << ": " << pow(x,0.2) << std::endl;
+	std::cout << "int, occupies " << k << "bit(a), min. value = " << mink << ", max. value = " << maxk << std::endl;
+
+}
